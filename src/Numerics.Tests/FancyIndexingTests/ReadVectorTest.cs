6e95f54178f475826f69f8822242e4b9e2e3acf0
Marco Ross <marco.ross@philips.com>
2019-05-16 17:03:24 +0200

    read-only indexing of vectors with integer enumerables

---
+++ src/Numerics.Tests/FancyIndexingTests/ReadVectorTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using MathNet.Numerics.LinearAlgebra.Single;
+
+namespace MathNet.Numerics.Tests.FancyIndexingTests
+{
+    [TestFixture, Category("FancyIndexing")]
+    public class ReadVectorTest
+    {
+
+        [Test]
+        public void TestSimpleIntegerIndex()
+        {
+            var v = DenseVector.OfArray(new[] { 0f, 1f, 2, 3, 4f, 5f });
+            Assert.That(v[3], Is.EqualTo(3f));
+            Assert.That(v[1], Is.EqualTo(1f));
+        }
+
+        [Test]
+        public void TestIntegerEnumerableIndex()
+        {
+            var v = DenseVector.OfArray(new[] { 0f, 2f, 4f, 6f, 8f, 10f });
+            var r = v[new [] { 1, 5, 0 }];
+            Assert.That(r, Is.Not.Null);
+            Assert.That(r, Is.EqualTo(DenseVector.OfArray(new [] { 2f, 10f, 0f })));
+        }
+    }
+}
---
+++ src/Numerics/Vector.FancyIndexing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MathNet.Numerics.LinearAlgebra
+{
+    public abstract partial class Vector<T>
+    {
+
+        public Vector<T> this[IEnumerable<int> indices]
+        {
+            get
+            {
+                var idxArray = indices as int[] ?? indices.ToArray();
+                var target = Build.SameAs(this, idxArray.Length);
+                for (var i = 0; i < idxArray.Length; i++)
+                {
+                    target[i] = this[idxArray[i]];
+                }
+                return target;
+            }
+        }
+
+
+    }
+}
