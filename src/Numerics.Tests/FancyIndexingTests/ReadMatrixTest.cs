ecfa021b1064edce8ef665d88374f59ffd34d705
MarcoRoss84 <marco.ross@philips.com>
2019-05-20 18:17:35 +0200

    Refactored indexing methods into an Indexer class that is commonly used by Vectors and Matrices. Implemented getters for subrows and subcolumns in matrices.

---
+++ src/Numerics.Tests/FancyIndexingTests/ReadMatrixTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NUnit.Framework;
+using MathNet.Numerics.LinearAlgebra.Single;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace MathNet.Numerics.Tests.FancyIndexingTests
+{
+    [TestFixture, Category("FancyIndexing")]
+    public class ReadMatrixTest
+    {
+        [Test]
+        public void GetSimpleIntegerIndex()
+        {
+            var m = DenseMatrix.OfArray(new float[,] {
+                { 11, 12, 13 },
+                { 21, 22, 23 },
+                { 31, 32, 33 },
+                { 41, 42, 43 }
+            });
+            Assert.That(m[2, 1], Is.EqualTo(32));
+            Assert.That(m[0, 2], Is.EqualTo(13));
+            Assert.That(m[3, 0], Is.EqualTo(41));
+        }
+
+        [Test]
+        public void GetRowVector()
+        {
+            var m = DenseMatrix.OfArray(new float[,] {
+                { 11, 12, 13 },
+                { 21, 22, 23 },
+                { 31, 32, 33 },
+                { 41, 42, 43 }
+            });
+            var row = m[2, Indexing.All];
+            Assert.That(row, Is.Not.Null);
+            Assert.That(row, Is.EqualTo(DenseVector.OfArray(new float[] { 31, 32, 33 })));
+        }
+
+        [Test]
+        public void GetRowSubVector()
+        {
+            var m = DenseMatrix.OfArray(new float[,] {
+                { 11, 12, 13 },
+                { 21, 22, 23 },
+                { 31, 32, 33 },
+                { 41, 42, 43 }
+            });
+            var row = m[1, new[] {0, 2}];
+            Assert.That(row, Is.Not.Null);
+            Assert.That(row, Is.EqualTo(DenseVector.OfArray(new float[] { 21, 23 })));
+        }
+
+        [Test]
+        public void GetColumnSubVector()
+        {
+            var m = DenseMatrix.OfArray(new float[,] {
+                { 11, 12, 13 },
+                { 21, 22, 23 },
+                { 31, 32, 33 },
+                { 41, 42, 43 }
+            });
+            var col = m[new[] { true, false, false, true}, 2];
+            Assert.That(col, Is.Not.Null);
+            Assert.That(col, Is.EqualTo(DenseVector.OfArray(new float[] { 13, 43 })));
+        }
+
+    }
+}
---
+++ src/Numerics/LinearAlgebra/Matrix.Indexing.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathNet.Numerics.LinearAlgebra
+{
+    public abstract partial class Matrix<T>
+    {
+
+        public Vector<T> this[int row, Indexer columns]
+        {
+            get {
+                columns.Init(ColumnCount);
+                var target = Vector<T>.Build.Dense(columns.Count);
+                for (var i = 0; i < columns.Count; i++)
+                {
+                    target[i] = this[row, columns[i]];
+                }
+                return target;
+            }
+        }
+
+        public Vector<T> this[Indexer rows, int column]
+        {
+            get
+            {
+                rows.Init(RowCount);
+                var target = Vector<T>.Build.Dense(rows.Count);
+                for (var i = 0; i < rows.Count; i++)
+                {
+                    target[i] = this[rows[i], column];
+                }
+                return target;
+            }
+        }
+
+    }
+}
